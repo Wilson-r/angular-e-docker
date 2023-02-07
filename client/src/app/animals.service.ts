fd0528320330fc3a94c471a4374d724e29e208ba
Wilson-r <wilson.riccardo@iisgalvanimi.edu.it>
2023-02-07 10:45:32 +0000

    update

--- client/src/app/animals.service.ts
+++ client/src/app/animals.service.ts
@@ -12,7 +12,7 @@ export class AnimalsService {
   
   //Il metodo fa una chiamata Http al server
   getAnimals() {
-    return this.http.get<VettAnimal>(environment.baseUrlServer + 'animals');
+    return this.http.get<VettAnimal>('/api/animals');
   }
 
   httpOptions = {
@@ -20,7 +20,7 @@ export class AnimalsService {
   };
   sendNewAnimal(animal : Animal) : Observable<Animal>
   {
-    return this.http.post<Animal>(environment.baseUrlServer + 'newAnimal', animal,this.httpOptions)
+    return this.http.post<Animal>('/api/newAnimal' + 'newAnimal', animal,this.httpOptions)
   }
 }
 
